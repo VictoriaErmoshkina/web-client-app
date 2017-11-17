24596ea560141e9affec6267e4c0a276e19f1195
Ermoshkina Victoria <ezhevika.96@gmail.com>
2017-11-17 22:19:26 +0300

    Buttons were edited. JSON is formatted in output.

--- FHIR UI/Models/JsonTextModel.cs
+++ FHIR UI/Models/JsonTextModel.cs
@@ -31,8 +31,30 @@ namespace FHIR_UI.Models
             this.ResType_ = type;
             this.ResId_ = id;
             this.ResVersion_ = version;
-            this.JsonText_ = text;
+            this.JsonText_ = FormatJson(text);
+            
         }
+
+        private const string INDENT_STRING = "    ";
+        private string FormatJson(string json)
+        {
+            int indentation = 0;
+            int quoteCount = 0;
+            var result =
+                from ch in json
+                let quotes = ch == '"' ? quoteCount++ : quoteCount
+                let lineBreak = ch == ',' && quotes % 2 == 0 ? ch + Environment.NewLine + String.Concat(Enumerable.Repeat(INDENT_STRING, indentation)) : null
+                let openChar = ch == '{' || ch == '[' ? ch + Environment.NewLine + String.Concat(Enumerable.Repeat(INDENT_STRING, ++indentation)) : ch.ToString()
+                let closeChar = ch == '}' || ch == ']' ? Environment.NewLine + String.Concat(Enumerable.Repeat(INDENT_STRING, --indentation)) + ch : ch.ToString()
+                select lineBreak == null
+                            ? openChar.Length > 1
+                                ? openChar
+                                : closeChar
+                            : lineBreak;
+
+            return String.Concat(result);
+        }
+
         public JsonTextModel()
         {
 
